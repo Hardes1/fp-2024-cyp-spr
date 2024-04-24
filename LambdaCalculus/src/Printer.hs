b4211356e1b226cb29ef6094eb18813c871b1f27
Georgii Ustinov <ustinovgo@gmail.com>
2024-04-24 15:09:21 +0300

    Implement pretty printer of lambda expression

---
+++ LambdaCalculus/src/Printer.hs
@@ -0,0 +1,17 @@
+module Printer(printTerm) where
+
+import Term (Term(..))
+import Text.Printf (printf)
+
+printTerm :: Term -> String
+printTerm (Variable name) = printf "%s" [name]
+printTerm (Lambda name body) = printf "\\%s.%s" [name] (printTerm body)
+printTerm (Application le@(Variable _) ri@(Variable _)) = printf "%s %s" (printTerm le) (printTerm ri)
+printTerm (Application app@(Application _ _) var@(Variable _)) = printf "%s %s" (printTerm app) (printTerm var)
+printTerm (Application lambda@(Lambda _ _) var@(Variable _)) = printf "(%s) %s" (printTerm lambda) (printTerm var)
+printTerm (Application var@(Variable _) app@(Application _ _)) = printf "%s (%s)" (printTerm var) (printTerm app)
+printTerm (Application lambda@(Lambda _ _) app@(Application _ _)) = printf "(%s) %s" (printTerm lambda) (printTerm app)
+printTerm (Application appLe@(Application _ _) appRi@(Application _ _)) = printf "(%s) (%s)" (printTerm appLe) (printTerm appRi)
+printTerm (Application rest lambda@(Lambda _ _)) = printf "%s (%s)" (printTerm rest) (printTerm lambda)
+
+
